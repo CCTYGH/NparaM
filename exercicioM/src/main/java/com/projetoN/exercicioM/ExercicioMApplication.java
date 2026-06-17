1aae6a81c2486089006da127d5ab5be9dbc1b36b
CCTYGH <ccctyg126@gmail.com>
2026-06-17 14:45:07 -0300

    criando

---
+++ exercicioM/src/main/java/com/projetoN/exercicioM/ExercicioMApplication.java
@@ -0,0 +1,13 @@
+package com.projetoN.exercicioM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioMApplication.class, args);
+	}
+
+}
---
+++ exercicioM/src/test/java/com/projetoN/exercicioM/ExercicioMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoN.exercicioM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
